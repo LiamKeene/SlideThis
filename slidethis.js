12cd7026149d04ab91e9443f7f4b2d1df0ba523f
Liam Keene <liam.keene@gmail.com>
2013-05-22 10:37:42 +1000

    Added default settings for the plugin
     * modified slidethis.js

--- slidethis.js
+++ slidethis.js
@@ -4,11 +4,22 @@ Copyright (c) 2013 Liam Keene
 Available under the MIT License
 */
 (function($) {
+    $.fn.slidethis = function(options) {
 
-    $.fn.slidethis = function() {
+        // Default settings
+        var settings = $.extend({
+            'auto': true,       // Boolean: automatically animate slides (true)
+            'pager': true,      // Boolean: show pager for slides (true)
+            'speed': 500,       // Integer: time (in ms) of transition (500)
+            'timeout': 5000,    // Integer: time (in ms) between transitions (5000)
+            'width': 600,       // Integer: width (in px) of the slideshow (600)
+            'height': 300,      // Integer: height (in px) of the slideshow (300)
+        }, options);
 
-        // Future home of SlideThis
+        return this.each(function() {
+            // Future home of SlideThis
+        });
 
-    }
+    };
 
 }(jQuery));
